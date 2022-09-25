f3fd28c3ebe957cbee25c454521797b987788770
charlie-ward <59178341+Charlie-Ward@users.noreply.github.com>
2022-09-25 15:14:47 +0100

    BROKEN

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { build } from 'esbuild'
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'b82f7cf17cmshd13f733707d454fp1c87b8jsn589c11fda68b')
+
+            return headers
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+        getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+        getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` })
+    }),
+})
+
+export const{
+    useGetTopChartsQuery,
+    useGetSongDetailsQuery,
+    useGetSongRelatedQuery,
+} = shazamCoreApi
